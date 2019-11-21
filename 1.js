7678ee81c32eb90d4efb44b1fda9f28473e17b8f
Seung Park <park1215@yahoo.com>
2019-11-20 22:51:57 -0700

    Nov 20 commit

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
-console.log("hello world");
+// console.log("hello world");
 
-const http = require('http');
+// const http = require('http');
 
 // http.createServer((request, response)=>{
 //     response.writeHead(200, {'Content-Type':'text/html'});
@@ -11,15 +11,15 @@ const http = require('http');
 
 let a = 10
 const b = 20
-console.log(a)
-console.log(b)
+// console.log(a)
+// console.log(b)
 
 const d = undefined
 
-console.log(`fileName : ${__filename}`)
-console.log(`dirName : ${__dirname}`)
+// console.log(`fileName : ${__filename}`)
+// console.log(`dirName : ${__dirname}`)
 
-console.time('alpha')
+// console.time('alpha')
 
 let output  = 1
 for(let i = 0; i <=10; i++){
@@ -27,6 +27,37 @@ for(let i = 0; i <=10; i++){
 }
 // console.log(`Result : ${output}`)
 
-console.timeEnd('alpha')
+// console.timeEnd('alpha')
 
-console.log(process)
+// console.log(process)
+
+// const http = require('http');
+const fs = require('fs')
+
+// http.createServer((request, response)=>{
+//     fs.readFile('btm8.mps', (error , file)=>{
+//         // if(error)
+//     response.writeHead(200, {'Content-Type': 'audio/mp3',
+//     'Set-Cookie':[
+//         'breakfast=toast',
+//         'dinner=chiken'
+//     ]
+// });
+//     response.end(file);
+// });
+// }).listen(52273, ()=>{
+//     console.log('Server running at ...');
+// })
+
+const http = require('http');
+const server = http.createServer();
+server.on('request', ()=>{
+    console.log('request event occurred');
+
+})
+server.on('connection', ()=>{
+    console.log('connection event occurred')
+})
+server.listen(52273, () => {
+    console.log('Server Running at http://127.0.0.1:52273');
+})
